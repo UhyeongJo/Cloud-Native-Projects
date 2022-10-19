11f643c61c6f414464e71d370a3ecd8aa6ee837b
uhyeong <uh9222959@gmail.com>
2022-10-19 22:31:34 +0900

    new add

--- src/main/java/com/example/simpleapplication/controller/HelloController.java
+++ src/main/java/com/example/simpleapplication/controller/HelloController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @GetMapping()
+    public String nothing(){
+        return "nothing!";
+    }
     @GetMapping("hello")
     public String hello(){
         return "Hello the new World!";
     }
+
 }
